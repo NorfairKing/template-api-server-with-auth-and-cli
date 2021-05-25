96ccd3e8dfd0449089d23e76b44fbb7dd62a9e74
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-05-25 09:44:47 +0200

    split up the nixos test to be more realistic

--- nix/nixos-module-test.nix
+++ nix/nixos-module-test.nix
@@ -9,35 +9,41 @@ in
 pkgs.nixosTest (
   { lib, pkgs, ... }: {
     name = "foo-bar-module-test";
-    machine = {
-      imports = [
-        foo-bar-production
-        home-manager
-      ];
-      services.foo-bar.production = {
-        enable = true;
-        api-server = {
+    nodes = {
+      server = {
+        imports = [
+          foo-bar-production
+        ];
+        services.foo-bar.production = {
           enable = true;
-          inherit port;
+          api-server = {
+            enable = true;
+            inherit port;
+          };
         };
       };
-      users.users.testuser.isNormalUser = true;
-      home-manager = {
-        useGlobalPkgs = true;
-        users.testuser = { pkgs, ... }: {
-          imports = [
-            ./home-manager-module.nix
-          ];
-          xdg.enable = true;
-          home.stateVersion = "20.09";
-          programs.foo-bar = {
-            enable = true;
-            fooBarPackages = pkgs.fooBarPackages;
-            sync = {
+      client = {
+        imports = [
+          home-manager
+        ];
+        users.users.testuser.isNormalUser = true;
+        home-manager = {
+          useGlobalPkgs = true;
+          users.testuser = { pkgs, ... }: {
+            imports = [
+              ./home-manager-module.nix
+            ];
+            xdg.enable = true;
+            home.stateVersion = "20.09";
+            programs.foo-bar = {
               enable = true;
-              server-url = "localhost:${builtins.toString port}";
-              username = "testuser";
-              password = "testpassword";
+              fooBarPackages = pkgs.fooBarPackages;
+              sync = {
+                enable = true;
+                server-url = "server:${builtins.toString port}";
+                username = "testuser";
+                password = "testpassword";
+              };
             };
           };
         };
@@ -46,23 +52,26 @@ pkgs.nixosTest (
     testScript = ''
       from shlex import quote
 
-      machine.wait_for_unit("multi-user.target")
+      server.start()
+      client.start()
+      server.wait_for_unit("multi-user.target")
+      client.wait_for_unit("multi-user.target")
 
-      machine.wait_for_open_port(${builtins.toString port})
-      machine.succeed("curl localhost:${builtins.toString port}")
+      server.wait_for_open_port(${builtins.toString port})
+      client.succeed("curl server:${builtins.toString port}")
 
-      machine.wait_for_unit("home-manager-testuser.service")
+      client.wait_for_unit("home-manager-testuser.service")
 
 
       def su(user, cmd):
           return f"su - {user} -c {quote(cmd)}"
 
 
-      machine.succeed(su("testuser", "cat ~/.config/foo-bar/config.yaml"))
+      client.succeed(su("testuser", "cat ~/.config/foo-bar/config.yaml"))
 
-      machine.succeed(su("testuser", "foo-bar register"))
-      machine.succeed(su("testuser", "foo-bar login"))
-      machine.succeed(su("testuser", "foo-bar greet"))
+      client.succeed(su("testuser", "foo-bar register"))
+      client.succeed(su("testuser", "foo-bar login"))
+      client.succeed(su("testuser", "foo-bar greet"))
     '';
   }
 )
